c098b8680e3aa7ffcbdcb2940a7adb9fe37413f3
Joe Groocock <me@frebib.net>
2021-02-05 09:06:39 +0000

    Add update.sh convenience script
    Makes updating to the latest Plex build a ton easier. The script pulls
    the latest build information, updates the Dockerfile, commits and pushes
    all automagically.
    
    [CI SKIP]
    
    Signed-off-by: Joe Groocock <me@frebib.net>

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+RELEASE="$(curl -fsSL https://api.spritsail.io/plex/release?pass | jq -c)"
+VERSION="$(jq -r .version <<< "$RELEASE")"
+CHECKSUM="$(jq -r '.["csum-deb"]' <<< "$RELEASE")"
+
+sed -Ei \
+    -e "s/^(ARG PLEX_VER=).*$/\1$VERSION/" \
+    -e "s/^(ARG PLEX_SHA=).*$/\1$CHECKSUM/" \
+    Dockerfile
+
+if ! git diff --quiet --exit-code Dockerfile; then
+    git reset --soft
+    git add -- Dockerfile
+    git commit \
+        --author="Spritsail Bot <bot@spritsail.io>" \
+        --no-gpg-sign \
+        -m "Update to Plex ${VERSION%-*}"
+    git push origin HEAD
+else
+    >&2 echo No update available
+fi
